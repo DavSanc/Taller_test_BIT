5b502043bf27cd2c447301ba8d2ecfb1e37fa7a8
David Sanchez <davidendubai@gmail.com>
2023-07-06 13:35:38 -0500

    [+] Primer ejercicio taller nota BIT

---
+++ main.js
@@ -0,0 +1,17 @@
+//*****************TALLER DE EJERCICIOS BIT 26/06/2023************/
+
+
+// 1. Valor: 0.5 - ¿Va a sobrevivir?
+// Un héroe se dirige al castillo para completar su misión. Sin embargo, le han dicho que el castillo está rodeado 
+//por un par de poderosos dragones. 
+//cada dragón necesita 2 balas para ser derrotado, nuestro héroe no tiene idea de cuántas balas debe cargar.
+
+function vaAsobrevivir(numeroDragones, numeroBalas) {
+    let balasNecesarias = numeroDragones * 2;
+
+    if (numeroBalas >= balasNecesarias){
+        return true;
+    } else{
+        return false
+    }
+}
